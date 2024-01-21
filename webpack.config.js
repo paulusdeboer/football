845452a7d13f8fbe4bb7b0aa789a6b1b91dfa2e9
Paulus de Boer <paulusdeboer@develto.nl>
2024-01-21 17:28:34 +0100

    bootstrap and other stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require("path");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -54,7 +55,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -68,6 +69,9 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enableStimulusBridge('./assets/controllers.json')
+
 ;
 
 module.exports = Encore.getWebpackConfig();
