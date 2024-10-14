994836af7d00f72acb40c560a51b1d26167fcda2
Paulus de Boer <paulusdeboer@develto.nl>
2024-10-14 20:49:34 +0200

    user and player seeder in one

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             PlayerSeeder::class,
             GameSeeder::class,
             RatingSeeder::class,
--- database/seeders/PlayerSeeder.php
+++ database/seeders/PlayerSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Player;
 use App\Models\User;
 use Database\Seeders\Helper\BaseSeeder;
+use Illuminate\Support\Facades\Hash;
 
 class PlayerSeeder extends BaseSeeder
 {
@@ -16,15 +17,24 @@ class PlayerSeeder extends BaseSeeder
     public function execute(): void
     {
         $lastPlayerId = Player::max('id') ?? 0;
-        $userIdMax = User::max('id') ?? 0;
+        $lastUserId = User::max('id') ?? 0;
 
         for ($i = 0; $i < self::PLAYER_COUNT; $i++) {
+            $user = User::create([
+                'id' => $lastUserId + $i + 1,
+                'name' => $this->fakerService->name(),
+                'email' => $this->fakerService->unique()->safeEmail(),
+                'password' => Hash::make('password'),
+                'created_at' => $this->fakerService->dateTimeBetween('-1 year'),
+                'updated_at' => $this->fakerService->dateTimeBetween('-1 year'),
+            ]);
+
             $this->add([
                 'id' => $lastPlayerId + $i + 1,
-                'name' => $this->fakerService->name(),
+                'name' => $user->name,
                 'rating' => $this->fakerService->numberBetween(500, 1000),
                 'type' => $this->fakerService->randomElement(['attacker', 'defender', 'both']),
-                'user_id' => $this->fakerService->numberBetween(1, $userIdMax),
+                'user_id' => $user->id,
                 'created_at' => $this->fakerService->dateTimeBetween('-1 year'),
                 'updated_at' => $this->fakerService->dateTimeBetween('-1 year'),
             ], Player::class);
