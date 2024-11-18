cdddbe92c2eb21707c0d42c0827be6d8ab41d014
Paulus de Boer <paulusdeboer@develto.nl>
2024-11-18 09:51:00 +0100

    better migrate

--- database/seeders/Migrate/PlayersMigrateSeeder.php
+++ database/seeders/Migrate/PlayersMigrateSeeder.php
@@ -15,7 +15,20 @@ final class PlayersMigrateSeeder extends Seeder
      */
     public function run(ConnectionInterface $connection): void
     {
-        $players = $connection->table('spelers')->get();
+        $players = $connection
+            ->table('spelers', 's')
+            ->select([
+                's.*',
+                'w.team',
+                'w.score',
+                'u.voor',
+                'u.tegen',
+            ])
+            ->leftJoin('wedstrijd as w', 's.id', '=', 'w.speler_id')
+            ->leftJoin('uitslag as u', 'w.datum', '=', 'u.datum')
+            ->orderBy('w.datum', 'DESC');
+
+        dump($players);die;
 
         if ($players->isEmpty()) {
             $this->command->info('No players found in the old database.');
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,9 +3,9 @@ set -x
 
 DBEXISTS=$(mysql -u root -p${ROOT_PASSWORD} -h mariadb --batch --skip-column-names -e "SHOW DATABASES LIKE 'migrate';" | grep migrate > /dev/null; echo "$?")
 if [ $DBEXISTS -eq 1 ];then
-    mysql -u root -p${ROOT_PASSWORD} -h mariadb -e "CREATE DATABASE migrate; GRANT ALL PRIVILEGES ON migrate.* TO '${DB_USERNAME}'@'%';"
+    mysql -u root -p${ROOT_PASSWORD} -h mariadb -e "CREATE DATABASE migrate;"
+    mysql -u root -p${ROOT_PASSWORD} -h mariadb -e "GRANT ALL PRIVILEGES ON migrate.* TO 'laravel'@'%';"
+    mysql -u root -p${ROOT_PASSWORD} -h mariadb migrate < database/dumps/migrate.sql
 fi
-
-mysql -u root -p${ROOT_PASSWORD} -h mariadb migrate < database/dumps/migrate.sql
 
 php artisan seed:from-old-db migrate
