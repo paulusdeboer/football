b91c059d1f512cf26b0525380e6940a7f7f1c2f6
Paulus de Boer <paulusdeboer@develto.nl>
2025-02-10 22:50:06 +0100

    change page title on auth pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }} - Register</title>
+        <title>{{ __('app_name') }}</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.bunny.net">
