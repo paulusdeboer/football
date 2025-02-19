ef6da3f14c205939428e5e920b3d0f8eefb2bbe6
Paulus de Boer <paulusdeboer@develto.nl>
2025-02-19 12:44:04 +0100

    add favicon to auth pages

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -7,6 +7,8 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
         <title>{{ __('app_name') }}</title>
 
         <!-- Fonts -->
