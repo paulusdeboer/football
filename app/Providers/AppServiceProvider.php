e82110154a5adcd61c8c60d00200909bbdf6929d
Paulus de Boer <paulusdeboer@develto.nl>
2025-02-19 11:39:30 +0100

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if (app()->environment('testing', 'staging')) {
+        if (app()->environment('local', 'testing')) {
             Mail::alwaysTo('paulusdeboer8@gmail.com');
         }
     }
