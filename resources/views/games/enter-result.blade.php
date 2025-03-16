c529f40a18afb2188e2ab07806ad4f4e1c6937aa
Paulus de Boer <paulusdeboer@develto.nl>
2025-03-16 22:19:58 +0100

    fix: default checked

--- resources/views/games/enter-result.blade.php
+++ resources/views/games/enter-result.blade.php
@@ -13,12 +13,17 @@
         <div class="card-header">{{ __('Enter game result') }}</div>
 
         <div class="card-body">
-            <form method="POST" id="game-form" action="{{ route('games.store-result', $game) }}">
+            <form method="POST" id="game-form"
+                  action="{{ route('games.store-result', $game) }}">
                 @csrf
 
                 <div class="form-check mb-3">
-                    <label class="form-check-label" for="send_rating_requests">{{ __('Send rating request e-mails') }}</label>
-                    <input type="checkbox" id="send_rating_requests" name="send_rating_requests" class="form-check-input" @if ($game->ratingRequests()->exists()) disabled @endif>
+                    <label class="form-check-label"
+                           for="send_rating_requests">{{ __('Send rating request e-mails') }}</label>
+                    <input type="checkbox" id="send_rating_requests"
+                           name="send_rating_requests" class="form-check-input"
+                           @if ($game->ratingRequests()->exists()) disabled
+                           @else checked @endif>
                 </div>
 
                 <div class="row">
@@ -43,18 +48,28 @@
 
                 <div class="row">
                     <div class="col-3 form-group">
-                        <label for="team1_score">{{ __('Team 1 score') }}</label>
-                        <input @if ($game->team1_score) value="{{ $game->team1_score }}" @endif type="number" id="team1_score" name="team1_score" class="form-control" required>
+                        <label
+                            for="team1_score">{{ __('Team 1 score') }}</label>
+                        <input
+                            @if ($game->team1_score) value="{{ $game->team1_score }}"
+                            @endif type="number" id="team1_score"
+                            name="team1_score" class="form-control" required>
                     </div>
 
                     <div class="col-3 form-group">
-                        <label for="team2_score">{{ __('Team 2 score') }}</label>
-                        <input @if ($game->team2_score) value="{{ $game->team2_score }}" @endif type="number" id="team2_score" name="team2_score" class="form-control" required>
+                        <label
+                            for="team2_score">{{ __('Team 2 score') }}</label>
+                        <input
+                            @if ($game->team2_score) value="{{ $game->team2_score }}"
+                            @endif type="number" id="team2_score"
+                            name="team2_score" class="form-control" required>
                     </div>
                 </div>
             </form>
-            <button type="submit" form="game-form" class="btn btn-primary mt-3">{{ __('Save result') }}</button>
+            <button type="submit" form="game-form"
+                    class="btn btn-primary mt-3">{{ __('Save result') }}</button>
         </div>
     </div>
-    <a href="{{ route('games.index') }}" class="btn btn-secondary">{{ __('Back to games list') }}</a>
+    <a href="{{ route('games.index') }}"
+       class="btn btn-secondary">{{ __('Back to games list') }}</a>
 @endsection
